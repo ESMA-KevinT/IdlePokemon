34da71ae9e0d1e6b1c5cab09c4f668e74bb47702
ESMA-KevinT <k.tribouillois@rns.ecolescreatives.com>
2024-11-22 16:58:26 +0100

    debut

---
+++ Assets/TRIBOUILLOIS_Kevin/Comics.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="new comics", menuName ="Comics", order = 0)]
+public class Comics : ScriptableObject
+{
+    
+}
